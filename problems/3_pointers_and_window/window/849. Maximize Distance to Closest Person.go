4ff835d7fca8ed51ec8dcb4cdee24634fdc8792e
arkosh <trukan24@gmail.com>
2024-11-26 17:28:52 +0300

    sliding window 849

--- problems/3_pointers_and_window/window/849. Maximize Distance to Closest Person.go
+++ problems/3_pointers_and_window/window/849. Maximize Distance to Closest Person.go
@@ -22,5 +22,28 @@ Output: 1
 */
 
 func maxDistToClosest(seats []int) int {
+	var l, r, result int
+	for l < len(seats) {
+		for r+1 < len(seats) && seats[r] == seats[r+1] { // смещаем правый указатель определяя окно последовательных чисел
+			r += 1
+		}
 
+		if seats[r] == 0 { // пересчитываем ответ, только если места были 0
+			// если 0 прижат к стенке слева или справа, т. е.
+			// 1 0 0 0
+			//   l   r
+
+			if l == 0 || r == len(seats)-1 { // мы у края, считаем по краевой формуле
+				result = max(result, r-l+1)
+			} else {
+				result = max(result, (r-l+2)/2)
+			}
+		}
+
+		// т.к. окно было закрыто (новое значение в ячейке), то смещаем оба указателя вперед на +1
+		l = r + 1
+		r = r + 1
+	}
+
+	return result
 }
